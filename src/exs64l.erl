3571bc4cc3a68221691c6427231bfbf6b2d63576
Kenji Rikitake <kenji.rikitake@acm.org>
2014-09-23 16:53:45 +0900

    Add exs64l.erl for xorshift*64 internal generator

---
+++ src/exs64l.erl
@@ -0,0 +1,79 @@
+%% @author Kenji Rikitake <kenji.rikitake@acm.org>
+%% @copyright 2014 Kenji Rikitake
+%% @doc Xorshift64star for Erlang (lightweight version)
+%% @end
+%% (MIT License)
+%%
+%% Copyright (c) 2014 Kenji Rikitake. All rights reserved.
+%%
+%% Permission is hereby granted, free of charge, to any person obtaining a copy of
+%% this software and associated documentation files (the "Software"), to deal in
+%% the Software without restriction, including without limitation the rights to
+%% use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+%% of the Software, and to permit persons to whom the Software is furnished to do
+%% so, subject to the following conditions:
+%%
+%% The above copyright notice and this permission notice shall be included in all
+%% copies or substantial portions of the Software.
+%%
+%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+%% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+%% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+%% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+%% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+%% SOFTWARE.
+%%
+
+-module(exs64l).
+
+-export([
+     gen1024/1,
+     next/1
+ ]).
+
+-export_type([
+        state/0,
+        uint64/0
+    ]).
+
+%% @type uint64(). 64bit unsigned integer type.
+
+-type uint64() :: 0..16#ffffffffffffffff.
+
+%% @type state(). Internal state data type for exs64.
+%% Internally represented as the record <code>#state{}</code>,
+%% of the 128bit seed.
+
+-opaque state() :: uint64().
+
+-define(UINT64MASK, 16#ffffffffffffffff).
+
+%% @doc Advance xorshift64star state for one step.
+%% and generate 64bit unsigned integer from
+%% the xorshift64star internal state.
+
+-spec next(state()) -> {uint64(), state()}.
+
+next(R) ->
+    R1 = R bxor (R bsr 12),
+    R2 = R1 bxor ((R1 bsl 25) band ?UINT64MASK),
+    R3 = R2 bxor (R2 bsr 27),
+    {(R3 * 2685821657736338717) band ?UINT64MASK, R3}.
+
+%% @doc Generate a list of 16 64-bit element list
+%% of the xorshift64star random sequence
+%% from a given 64-bit seed.
+
+-spec gen1024(state()) -> list(uint64()).
+
+gen1024(R) ->
+    gen1024(16, R, []).
+
+-spec gen1024(non_neg_integer(), state(), list(uint64())) -> list(uint64()).
+
+gen1024(0, _, L) ->
+    L;
+gen1024(N, R, L) ->
+    {X, R2} = next(R),
+    gen1024(N - 1, R2, [X|L]).
